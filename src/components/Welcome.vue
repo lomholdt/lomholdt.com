dbba2a3b0c2f1948e19162e1d5da9c09a11435fb
lomholdt <jonaslomholdt@gmail.com>
2018-01-27 22:10:12 +0100

    Initial commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <img src="../assets/me.jpg" id="Lomholdt__Me">
+    <h1>{{ msg }}</h1>
+    <h2>About Me</h2>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/jonaslomholdt/">
+            LinkedIn
+        </a>
+      </li>
+      <li>
+
+      </li>
+      <li>
+
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/jonaslomholdt"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#Lomholdt__Me {
+  width: 125px;
+  border-radius: 100px
+}
+</style>
